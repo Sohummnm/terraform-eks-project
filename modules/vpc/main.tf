62b1af918c72c17cbc561382f04083ebd40c4ebd
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-29 00:00:24 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,15 +1,21 @@
+data "aws_availability_zones" "available" {
+  state = "available" 
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   environment = var.environment
-  cidr = "10.0.0.0/16"
+  cidr = var.cidr_block
   name = "my-${var.environment}-vpc
 
-  azs             = ["eu-west-1a"]
+  azs             = slice(data.aws_availability_zones.available.names[0])
   private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
     Terraform = "true"
