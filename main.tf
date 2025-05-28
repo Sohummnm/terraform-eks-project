1429c842920230a789698bed414943730db75d3d
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-28 23:06:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
+provider "aws" {
+  region = "var.region"
+}
 
+module "vpc" {
+  source = "./modules/vpc"
+  cidr_block = var.vpc_cidr
+  environment = var.environment
+}
+
+module "iam" {
+  source = "./modules/iam"
+  cluster_name = var.cluster_name
+}
+
+module "eks" {
+  source = "./modules/eks"
+  cluster_name = var.cluster_name
+  vpc_id = module.vpc.vpc_id
+  eks_version = var.eks_version
+  subnet_ids = module.vpc.private_subnets
+  environment = var.environment
+}
