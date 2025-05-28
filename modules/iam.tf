0703c4aca3972baed4575a361beafd11399288e3
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-28 23:26:37 +0530

    Update iam.tf

--- modules/iam.tf
+++ modules/iam.tf
@@ -1,4 +1,9 @@
-module "iam" {
-  source = "./modules/iam"
-  cluster_name = var.cluster_name
+module "iam_eks_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-eks-role"
+
+  role_name = "${var.cluster_name}-${var.environment}-eks-cluster-role"
+
+  cluster_service_accounts = {
+    "${var.cluster_name}" = ["default:my-serviceaccount-${var.environment}"]
+  }
 }
