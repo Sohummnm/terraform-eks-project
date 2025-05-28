2348700b89b0e89ee5dde417bec82c2e6821aa13
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-28 21:39:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region = var.region
+}
 
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"
+    key = "envs/${var.environment}/terraform.tfstate"
+    region = var.region
+    dynamodb_table = "terraform-locks"
+}
+}
+
+module
