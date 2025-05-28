ea3b5ac0a3a88161a64807d141f385217100f5dd
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-29 00:19:45 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -10,7 +10,7 @@ module "eks" {
       desired_capacity = 2
       max_capacity = 3
       min_capacity = 1
-      instance_types = ["t3.medium"]
+      instance_types = ["${var.instance_type}"]
 }
 }
   tags = {
