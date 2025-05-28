def19cf1717de548b3ad614692509527a346a208
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-29 00:37:29 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets.id
+  value = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-  value = module.vpc.public_subnets.id
+  value = module.vpc.public_subnets
 }
