de154586161d9f78ae5f179ec4bfe8a58eba5e63
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-28 20:59:57 +0530

    Update vpc.tf

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-eks-vpc"
+  source = "./modules/vpc"
+  environment = var.environment
   cidr = "10.0.0.0/16"
+  name = "my-${var.environment}-vpc
 
   azs             = ["eu-west-1a"]
   private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  public_subnets  = ["10.0.2.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
