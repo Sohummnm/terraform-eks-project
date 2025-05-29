cbcb5c49b865d12d57e558549e186ebe7825f9f2
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-29 18:43:02 +0530

    Update variables.tf

--- modules/eks-node-group/variables.tf
+++ modules/eks-node-group/variables.tf
@@ -4,5 +4,8 @@ variable "subnet_ids" { type = list(string) }
 variable "vpc_id" {}
 variable "environment" {}
 variable "instance_type" {}
-variable "cluster_service_cidr" {}
 variable "cluster_primary_sg_id" {}
+variable "cluster_service_cidr" {
+  type    = string
+  default = "172.20.0.0/16"
+}
