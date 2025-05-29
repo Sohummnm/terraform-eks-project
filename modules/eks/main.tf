2c8e146d055690415bd981d5eedf5cd9bcc95c2a
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-29 17:21:48 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -10,22 +10,3 @@ module "eks" {
     Terraform   = "true"
   }
 }
-
-module "eks_node_group" {
-  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
-  version = "~> 20.31"
-
-  cluster_name = module.eks.cluster_name
-  subnet_ids   = var.subnet_ids
-
-  name = "default"
-  desired_size = 2
-  min_size     = 1
-  max_size     = 3
-  instance_types = [var.instance_type]
-
-  tags = {
-    Environment = var.environment
-    Terraform   = "true"
-  }
-}
