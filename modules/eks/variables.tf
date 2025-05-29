959aa6a54d616e705753cb669989c6b70c8bd727
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-29 18:42:09 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -4,3 +4,5 @@ variable "subnet_ids" { type = list(string) }
 variable "vpc_id" {}
 variable "environment" {}
 variable "instance_type" {}
+variable "cluster_service_cidr" {}
+variable "cluster_primary_sg_id" {}
