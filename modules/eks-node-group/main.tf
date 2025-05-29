fb4daa70e333e6d5be052a47ee3fc2b497dc0382
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-29 17:39:09 +0530

    Update main.tf

--- modules/eks-node-group/main.tf
+++ modules/eks-node-group/main.tf
@@ -2,7 +2,7 @@ module "eks_node_group" {
   source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
   version = "~> 20.31"
 
-  cluster_name = module.eks.cluster_name
+  cluster_name = var.cluster_name
   subnet_ids   = var.subnet_ids
 
   name = "default"
