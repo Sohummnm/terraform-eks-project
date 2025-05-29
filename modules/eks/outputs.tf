3f9b2d7329ddd3bfaf1f635864481348669fdc97
Soham Kanoj <87384247+Sohummnm@users.noreply.github.com>
2025-05-29 18:45:59 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -9,3 +9,7 @@ output "cluster_name" {
 output "cluster_ca_certificate" {
   value = module.eks.cluster_certificate_authority_data
 }
+
+output "cluster_primary_security_group_id" {
+  value = module.eks.cluster_primary_security_group_id
+}
